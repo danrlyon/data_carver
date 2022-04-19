bcd668c23d00493bf2780273ace8461267c7dcec
Dan Albeit <danrlyon.github.io@gmail.com>
2022-04-18 20:55:39 -0600

    Update TODO plan and prepare repo.

--- data_carver.py
+++ data_carver.py
@@ -1,20 +1,34 @@
-'''Data Carver
+''' Data Carver
 
-Requirements:
-    Write a Python program to carve evidence from a binary file.
-        - There are no import library restrictions
-        - Your program must accept a binary file as a command-line argument (test 
-            using a binary file in the same folder as your python program)
-        - Your program must write carved files to a folder titled with your last name
-        - Your program must write the MD5 hash of each carved file to a file names
-            hashes.txt in the same folder as the carved images
-        - Your program must output to screen some basic file information such as
-            file type found, file size, and location offset for each carved file
+    Requirements:
+        Write a Python program to carve evidence from a binary file.
+            - There are no import library restrictions
+            - Your program must accept a binary file as a command-line argument (test 
+                using a binary file in the same folder as your python program)
+            - Your program must write carved files to a folder titled with your last name
+            - Your program must write the MD5 hash of each carved file to a file names
+                hashes.txt in the same folder as the carved images
+            - Your program must output to screen some basic file information such as
+                file type found, file size, and location offset for each carved file
 
-    Recommend you start by carving one file type, then expand your solution to carve various file types.
-    Your solution will be testing using a binary file with primarily jpg, png, and pdf file types.
+        Recommend you start by carving one file type, then expand your solution to carve various file types.
+        Your solution will be testing using a binary file with primarily jpg, png, and pdf file types.
 
 
+    TODO:
+        - Take input args
+        - Open File
+        - Find JPG Header
+        - Find JPG Footer
+        - Write the JPG to a file
+            - Into Folder titled with my last name
+            - Write the filenames and its hash to hashes.txt in same folder
+        - Print the following:
+            - file type
+            - file size
+            - start and finish offsets
+        - Repeat for PNG
+        - Repeat for PDF
 
 '''
 
